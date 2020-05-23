96ef39d42e0516f943787c677835a2f74034290f
John Doe <zeverai@codys-imac.home>
2020-05-23 11:28:26 -0400

    Create bug fixed. Worked out more psudo code.

--- views/Index.jsx
+++ views/Index.jsx
@@ -7,24 +7,37 @@ class Index extends React.Component {
         return (
             <Layout>
                 <div>
+
+                    <div className="index-header">
+                    <h1>Path of Exile - Character Database</h1>
+                    </div>
+
+                    <nav>
+                        <a href="/characters/new">Click Here to Add Character to Database</a>
+                    </nav>
+
                     <div className = "character-card-container">
                         {data.map((data, i)=> {
                             return (
                                 <div className="character-card" key="index"> 
                                     <h3>{data.name}</h3>
-                                    <div>
-                                        <img src={data.img} alt=""/>
-                                    </div>
+                                        <div>
+                                            <a href={`/characters/${data._id}`}><img src={data.img} alt={data.name}/></a>
+                                        </div>
                                     <p>{data.bio}</p>
+                                    <p></p>
                                 </div>
                             )}
                         )}
-                    
                     </div>
+
+
+
+
                 </div>
             </Layout>
         )
-    }
-}
+    };
+};
 
 module.exports = Index;
