f7b1ebec92d18db3fb64ac06596d94a3f4b4c5bb
John Doe <zeverai@codys-imac.home>
2020-05-28 03:06:55 -0400

    edit page css added

--- views/Edit.jsx
+++ views/Edit.jsx
@@ -9,11 +9,14 @@ class Edit extends React.Component {
             <div className="edit-container">
                 <h1 className="edit-header-text">Add Character to Database Using the Form Below</h1>
 
-                    <form action={`/characters/edit/${data._id}?_method=put`} method="POST">
-                        Name: <input type="text" name="name" value={data.name}/><br/>
-                        Bio: <textarea type="text" name="bio" value={data.bio}/><br/>
-                        Image: <input type="text" name="img" value={data.img}/><br/>
-                        <input type="submit" name="" value="Update Character"/>
+                    <form className="edit-form" action={`/characters/edit/${data._id}?_method=put`} method="POST">
+                        Name: <br/>
+                        <input type="text" name="name" defaultValue={data.name} size="40"/><br/>
+                        Bio: <br/>
+                        <textarea type="text" name="bio" defaultValue={data.bio} rows="16" cols="70"/><br/>
+                        Image: <br/>
+                        <input type="text" name="img" defaultValue={data.img} size="40"/><br/>
+                        <input type="submit" name="" defaultValue="Update Character"/>
                     </form>
             </div>
         </Layout>
