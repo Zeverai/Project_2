3bcc91ed254e003f957839a76a077fdf24924c8d
John Doe <zeverai@codys-imac.home>
2020-05-28 03:43:04 -0400

    new page fixed. bug found/fixed

--- views/New.jsx
+++ views/New.jsx
@@ -5,16 +5,19 @@ class New extends React.Component {
     render() {
       return (
           <Layout>
-                <div>
-                    <h1>Add Character Here:</h1>
-                        <nav>
-                            <a href="/characters">Return to Character Database</a>
-                        </nav>
-                    <form action="/characters" method="POST">
-                        Name: <input type="text" name="name" placeholder="NPC Name Here"/><br/>
-                        Bio: <textarea type="text" name="bio" placeholder="Biography Here"/><br/>
-                        Img: <input type="text" name="img" placeholder="IMG URL"/><br/>
-                        <input type="submit" name="" value="Add To Database"/>
+                <div className="edit-container">
+                    <h1 className="edit-header-text">Add New Character Here:</h1>
+
+                        <a className="return-link-edit-new" href="/characters">Return to Character Database</a>
+
+                            <form className="edit-form" action="/characters" method="POST">
+                                Name: <br/> 
+                                <input type="text" name="name" placeholder="NPC Name Here" size="40"/><br/>
+                                Bio: <br/> 
+                                <textarea type="text" name="bio" placeholder="Biography Here" rows="20" cols="70"/><br/>
+                                Img: <br/> 
+                                <input type="text" name="img" placeholder="IMG URL" size="40"/><br/>
+                            <input type="submit" name="" value="Add To Database" className="update-button"/>
                     </form>
                 </div>);
           </Layout>
