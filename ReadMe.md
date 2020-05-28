f5afac1185c4576b9bcfe5fcaa22a4065bb15da9
John Doe <zeverai@codys-imac.home>
2020-05-28 04:04:37 -0400

    fore more characters added

--- ReadMe.md
+++ ReadMe.md
@@ -8,3 +8,11 @@ Includes masters and important quest npc's from the main storyline,
 as well as epilogue and end game content characters.
 Designed to be a quick reference for any new players trying to get 
 an idea of who each NPC is, and how they interact with the story.
+
+
+Character descriptions from Path of Exile Wiki.
+```https://pathofexile.gamepedia.com/Path_of_Exile_Wiki```
+
+Art assets used from GGG promotional materials.
+Some assets edited for animation.
+```https://www.pathofexile.com/```
--- views/Edit.jsx
+++ views/Edit.jsx
@@ -8,16 +8,17 @@ class Edit extends React.Component {
         <Layout>
             <div className="edit-container">
                 <h1 className="edit-header-text">Add Character to Database Using the Form Below</h1>
-
-                    <form className="edit-form" action={`/characters/edit/${data._id}?_method=put`} method="POST">
-                        Name: <br/>
-                        <input type="text" name="name" defaultValue={data.name} size="40"/><br/>
-                        Bio: <br/>
-                        <textarea type="text" name="bio" defaultValue={data.bio} rows="20" cols="70"/><br/>
-                        Image: <br/>
-                        <input type="text" name="img" defaultValue={data.img} size="40"/><br/>
-                        <input className="update-button" type="submit" name="" defaultValue="Update Character"/>
-                    </form>
+                    <a className="return-link-edit-new" href="/characters">Return to Character Database</a>
+                    
+                        <form className="edit-form" action={`/characters/edit/${data._id}?_method=put`} method="POST">
+                            Name: <br/>
+                            <input type="text" name="name" defaultValue={data.name} size="40"/><br/>
+                            Bio: <br/>
+                            <textarea type="text" name="bio" defaultValue={data.bio} rows="20" cols="70"/><br/>
+                            Image: <br/>
+                            <input type="text" name="img" defaultValue={data.img} size="40"/><br/>
+                            <input className="update-button" type="submit" name="" defaultValue="Update Character"/>
+                        </form>
             </div>
         </Layout>
       )}
